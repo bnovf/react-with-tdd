aacda158ff3801d23e8ac265513c19743b8ce798
nfatah <nfatah41@gmail.com>
2018-02-01 12:39:46 +0300

    fix pug linting error

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,6 @@ const { injectBabelPlugin } = require('react-app-rewired')
 
 module.exports = function override(config, env) {
 	// add  plugins
-	config = injectBabelPlugin(['transform-react-pug', 'transform-react-pug'], config)
+	config = injectBabelPlugin(['transform-react-pug', 'transform-react-pug', 'react-pug'], config)
   return config
 }
