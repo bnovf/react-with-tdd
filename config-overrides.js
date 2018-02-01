26be6e54d33d27ff06ec5c2298edb27f7fc6b83f
nfatah <nfatah41@gmail.com>
2018-02-01 14:14:32 +0300

    stop rewire, just eject

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* config-overrides.js to help us configure webpack while not ejecting*/
-
-const { injectBabelPlugin } = require('react-app-rewired')
-
-module.exports = function override(config, env) {
-	// add  plugins
-	config = injectBabelPlugin(['transform-react-pug', 'transform-react-pug', 'react-pug'], config)
-  return config
-}
