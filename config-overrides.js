ce012844dfd1c5405c6c53c59644bc19da993117
nfatah <nfatah41@gmail.com>
2018-02-01 11:55:23 +0300

    Rewire CRA rather than ejecting.

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+/* config-overrides.js to help us configure webpack while not ejecting*/
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  return config;
+}
