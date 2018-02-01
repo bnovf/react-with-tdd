efa6a1c9a7d7a4b357b765c41c4b6d9a420352c0
nfatah <nfatah41@gmail.com>
2018-02-01 15:29:46 +0300

    add tests for pug react component

--- src/Tests/Helloworld.test.js
+++ src/Tests/Helloworld.test.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { HelloWorld } from '../components/components'
-import { shallow, configure } from 'enzyme'
+import { shallow, configure, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-
+import styledComponents from 'styled-components'
 // setup enzyme
 configure({ adapter: new Adapter() })
 
+// test paragraph
 it('should render a paragraph text "Hello, world"', () => {
-  const wrapper = shallow(<HelloWorld />)
-  expect(wrapper.text()).toEqual('Hello, world');
+  const wrapper = shallow(<HelloWorld />) // shallow rendering
+  expect(wrapper.text()).toEqual('Hello, world')
+})
+// test button
+it('should render a button from styled-components', () => {
+  const Button = styledComponents.button
+  const mounted = mount(<Button />) // fullDOM rendering
+  expect(mounted.find(<Button />).exists()).toEqual(true)
 })
--- src/components/components.js
+++ src/components/components.js
@@ -1,3 +1,9 @@
 import React from 'react'
+import styled from 'styled-components';
+const Button = styled.button``;
+
 // import pug from 'pug'
-export const HelloWorld = () => pug`p.App-intro Hello, world`
+export const HelloWorld = () => pug`
+p.App-intro Hello, world
+Button Pug Button that works
+`
